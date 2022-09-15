d3de76069d7d4ab7ac1455bd2e14eb340c61c127
Santiago <alexander.cardenas10@outlook.com>
2022-09-15 11:30:42 -0500

    Remove unnecessary

---
+++ src/components/Counter.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import {
+  decrement,
+  increment,
+  incrementByAmount,
+  incrementAsync,
+  incrementIfOdd,
+  selectCount,
+} from '../redux/counter/counterSlice';
+
+
+export function Counter() {
+  const count = useSelector(selectCount);
+  const dispatch = useDispatch();
+  const [incrementAmount, setIncrementAmount] = useState('2');
+
+  const incrementValue = Number(incrementAmount) || 0;
+
+  return (
+    <div>
+      <div>
+        <button onClick={() => dispatch(decrement())}>
+          -
+        </button>
+        <span>{count}</span>
+        <button onClick={() => dispatch(increment())}>
+          +
+        </button>
+      </div>
+      <div>
+        <input value={incrementAmount} onChange={(e) => setIncrementAmount(e.target.value)}/>
+        <button onClick={() => dispatch(incrementByAmount(incrementValue))}>
+          Add Amount
+        </button>
+        <button onClick={() => dispatch(incrementAsync(incrementValue))}>
+          Add Async
+        </button>
+        <button onClick={() => dispatch(incrementIfOdd(incrementValue))}>
+          Add If Odd
+        </button>
+      </div>
+    </div>
+  );
+}
