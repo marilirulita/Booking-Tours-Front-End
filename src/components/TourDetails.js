bdaa257b063a7df31647cbbe57ec450d0ba44382
Santiago <alexander.cardenas10@outlook.com>
2022-09-16 12:33:57 -0500

    Configure TourDetails redux file

---
+++ src/components/TourDetails.js
@@ -0,0 +1,44 @@
+import { React, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { GetTeamsAPI } from '../redux/tours/tours';
+
+const TourDetails = () => {
+  const num = 4;
+  const dispatch = useDispatch();
+  const Store = useSelector((store) => store.tours);
+  useEffect(() => {
+    document.title = dispatch(GetTeamsAPI(num));
+  }, []);
+  const tour = Store[0];
+  if (tour !== undefined) {
+    return (
+      <div className="tour-details">
+        <div className="tour-photo">
+          <img src={tour.photo} alt="Tour" />
+        </div>
+        <div className="tour-data">
+          <h2 className="tour-title">{tour.title}</h2>
+          <p className="tour-description">{tour.description}</p>
+          <div className="tour-city">
+            <p>City:</p>
+            <p>{tour.city}</p>
+          </div>
+          <div className="tour-duration">
+            <p>Duration (weeks):</p>
+            <p>{tour.duration}</p>
+          </div>
+          <div className="tour-cost">
+            <p>Cost:</p>
+            <p>{tour.cost}</p>
+          </div>
+          <p className="tour-available">Available Now</p>
+          <div className="tour-reserve-btn">
+            <button type="button">Reserve </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+  return false;
+};
+export default TourDetails;
