1c7f2f97797a4a58c2da608db810f8f2ba1f4586
Santiago <alexander.cardenas10@outlook.com>
2022-09-16 22:46:48 -0500

    Add Reservations Route

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import Main from './components/Main';
 import { Counter } from './components/Counter';
 import TourDetails from './components/TourDetails';
+import Reservations from './components/Reservations';
 import './styling/TourDetails.css';
 import './styling/main.css';
 
@@ -14,6 +15,7 @@ function App() {
           <Route path="/" element={<Main />} />
           <Route path="/counter" element={<Counter />} />
           <Route path="/TourDetails/:tourID" element={<TourDetails />} />
+          <Route path="/Reservations" element={<Reservations />} />
         </Routes>
       </header>
     </div>
