5a93324f988b6ae5ea55064d26049af5c26e693c
mar <marilirulita@gmail.com>
2022-09-19 14:22:10 -0700

    add state to manage form data

--- src/components/NewTour.js
+++ src/components/NewTour.js
@@ -1,17 +1,33 @@
-const NewTour = () => (
-  <div className="content">
-    <h1>Create a new Tour</h1>
-    <div className="newTourForm">
-      <input type="number" placeholder="User id" />
-      <input type="text" placeholder="Tour Title" />
-      <input type="number" placeholder="Duration" />
-      <input type="text" placeholder="Description" />
-      <input type="text" placeholder="City" />
-      <input type="number" placeholder="Cost" />
-      <input type="text" placeholder="Photo URL" />
-      <button type="submit" value="add-tour">Create Tour</button>
+import { useState } from 'react';
+
+const NewTour = () => {
+  const newTour = {
+    title: '',
+    duration: 0,
+    description: '',
+    city: '',
+    user_id: 0,
+    cost: 0,
+    photo: '',
+  };
+
+  const [state, setState] = useState(newTour);
+
+  return (
+    <div className="content">
+      <h1>Create a new Tour</h1>
+      <div className="newTourForm">
+        <input type="number" placeholder="User id" value={state.title} onChange={(e) => setState({ ...state, title: e.target.value })} />
+        <input type="text" placeholder="Tour Title" />
+        <input type="number" placeholder="Duration" />
+        <textarea placeholder="Description" />
+        <input type="text" placeholder="City" />
+        <input type="number" placeholder="Cost" />
+        <input type="text" placeholder="Photo URL" />
+        <button type="submit" value="add-tour">Create Tour</button>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default NewTour;
