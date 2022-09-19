fb918ceeeef8da5d4ff29829ecfedab257628ca2
Santiago <alexander.cardenas10@outlook.com>
2022-09-19 12:22:15 -0500

    Update reservations reducer file

--- src/redux/reservations/reservations.js
+++ src/redux/reservations/reservations.js
@@ -1,7 +1,7 @@
 const ADD_RESERVATION = 'ADD_RESERVATION';
 const REMOVE_RESERVATION = 'REMOVE_RESERVATION';
 const GET_API_DATA_RESERVATION = 'GET_API_DATA_RESERVATION';
-
+const URL = 'http://127.0.0.1:3000/user_tours';
 const initialState = [];
 
 // Action Creators
@@ -30,5 +30,29 @@ export const reservationReducer = (state = initialState, action) => {
       return [...action.payload];
     default:
       return state;
+  }
+};
+
+export const PostReservationsAPI = (data, user) => async () => {
+  const reservationData = {
+    user_id: user,
+    tour_id: data.tour_id,
+    reservation_date: data.reservation_date,
+    persons_number: data.persons_number,
+  };
+  const response = await fetch(URL, {
+    method: 'POST',
+    mode: 'cors',
+    cache: 'no-cache',
+    credentials: 'same-origin',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    redirect: 'follow',
+    referrerPolicy: 'no-referrer',
+    body: JSON.stringify(reservationData),
+  });
+  if (response.status === 201) {
+    console.log('Record created');
   }
 };
