911d837f973094d4a5e347547181c13f3ca53b5b
mar <marilirulita@gmail.com>
2022-09-19 14:01:47 -0700

    add new tour form basic form

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import { Routes, Route } from 'react-router-dom';
 import Main from './components/Main';
 import { Counter } from './components/Counter';
 import TourDetails from './components/TourDetails';
+import NewTour from './components/NewTour';
 import './styling/TourDetails.css';
 import './styling/main.css';
+import './styling/newTourForm.css';
 
 function App() {
   return (
@@ -14,6 +16,7 @@ function App() {
           <Route path="/" element={<Main />} />
           <Route path="/counter" element={<Counter />} />
           <Route path="/TourDetails/:tourID" element={<TourDetails />} />
+          <Route path="/NewTour" element={<NewTour />} />
         </Routes>
       </header>
     </div>
--- src/components/NewTour.js
+++ src/components/NewTour.js
@@ -1,46 +1,17 @@
-const NewTour = () => {
-  // const URLTOUR = 'http://127.0.0.1:3000/tours';
-  const URLRESER = 'http://127.0.0.1:3000/user_tours';
-
-  // const tourData = {
-  //   title: 'Test Tour',
-  //   duration: 4.0,
-  //   description: 'In this tour we will visit x and ...',
-  //   city: 'Testing',
-  //   user_id: 1,
-  //   cost: 500,
-  //   photo: 'https://cdn.mexicodestinos.com/tours/tour-a-chichen-itza-princ-min.jpg',
-  // };
-
-  const reservationData = {
-    user_id: 1,
-    tour_id: 3,
-    reservation_date: '2021-10-10',
-    persons_number: 4,
-  };
-
-  const postData = async (url, data) => {
-    const response = await fetch(url, {
-      method: 'POST',
-      mode: 'cors',
-      cache: 'no-cache',
-      credentials: 'same-origin',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      redirect: 'follow',
-      referrerPolicy: 'no-referrer',
-      body: JSON.stringify(data),
-    });
-    console.log(response);
-    return response.json();
-  };
-
-  return (
-    <div>
-      <button type="submit" onClick={() => postData(URLRESER, reservationData)}>Create Tour</button>
+const NewTour = () => (
+  <div className="content">
+    <h1>Create a new Tour</h1>
+    <div className="newTourForm">
+      <input type="number" placeholder="User id" />
+      <input type="text" placeholder="Tour Title" />
+      <input type="number" placeholder="Duration" />
+      <input type="text" placeholder="Description" />
+      <input type="text" placeholder="City" />
+      <input type="number" placeholder="Cost" />
+      <input type="text" placeholder="Photo URL" />
+      <button type="submit" value="add-tour">Create Tour</button>
     </div>
-  );
-};
+  </div>
+);
 
 export default NewTour;
