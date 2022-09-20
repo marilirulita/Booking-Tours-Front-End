89de7fabd02f0d34d770bad399cac2b5664fd86c
devMahmoud <eng.mahmoud.mohamed93@gmail.com>
2022-09-20 21:57:08 +0200

    Add sign up component

---
+++ src/components/SignUp.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { postUserApi } from '../redux/user/userAPI';
+
+const SignUp = () => {
+  /*
+  const signUp = {
+    name: '',
+    email: '',
+    password: '',
+  };
+
+  const [state, setState] = useState(signUp);
+  const dispatch = useDispatch();
+
+  const submitForm = () => {
+    dispatch(postUserApi(state));
+    setState(signUp);
+  };
+  */
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const dispatch = useDispatch();
+
+  const createUserHandeler = (event) => {
+    event.preventDefault();
+    if (!name || !email || !password) return;
+    const user = {
+      name,
+      email,
+      password,
+    };
+    dispatch(postUserApi(user));
+    setName('');
+    setEmail('');
+    setPassword('');
+  };
+
+  return (
+    <div className="content">
+      <h1>Sign Up</h1>
+      <form className="signUpForm" onSubmit={createUserHandeler}>
+        <input type="text" className="field" placeholder="Name" value={name} onChange={(e) => setName(e.target.value)} required />
+        <input type="text" className="field" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+        <input type="password" className="field" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+        <button type="submit" value="add-tour">Register</button>
+      </form>
+    </div>
+  );
+};
+
+export default SignUp;
