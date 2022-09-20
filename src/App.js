40cefc31ed70a98efd0efadc9af558b2434123ef
devMahmoud <eng.mahmoud.mohamed93@gmail.com>
2022-09-20 21:57:30 +0200

    Add sign up route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import SignUp from './components/SignUp';
 import Main from './components/Main';
 import { Counter } from './components/Counter';
 import TourDetails from './components/TourDetails';
@@ -7,6 +8,7 @@ import NewTour from './components/NewTour';
 import ReservationsForm from './components/ReservationsForm';
 import Reservations from './components/Reservations';
 import DeleteTours from './components/DeleteTours';
+import './styling/SignUp.css';
 import './styling/TourDetails.css';
 import './styling/main.css';
 import './styling/newTourForm.css';
@@ -18,6 +20,7 @@ function App() {
       <header className="App-header">
         <Routes>
           <Route path="/" element={<Main />} />
+          <Route path="/SignUp" element={<SignUp />} />
           <Route path="/counter" element={<Counter />} />
           <Route path="/TourDetails/:tourID" element={<TourDetails />} />
           <Route path="/NewTour" element={<NewTour />} />
