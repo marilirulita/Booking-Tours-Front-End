83d86ef87acf36ddc42957a364b7435fa3ec5174
Santiago <alexander.cardenas10@outlook.com>
2022-09-21 12:57:12 -0500

    Update get the token from the local storage

--- src/components/ReservationsForm.js
+++ src/components/ReservationsForm.js
@@ -1,13 +1,13 @@
 import { React, useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
 import { fetchApiDataTours } from '../redux/tours/toursAPI';
 import { PostReservationsAPI } from '../redux/reservations/reservations';
 import '../styling/ReservationsForm.css';
 
 const Reservations = () => {
-  const navigate = useNavigate();
-  const user = 1;
+  const user = JSON.parse(localStorage.getItem('user'));
+  const { token } = user;
+
   const [value, setValue] = useState({
     persons_number: 0, reservation_date: '', tour_id: 0, user_id: 0,
   });
@@ -19,13 +19,12 @@ const Reservations = () => {
   const dispatch = useDispatch();
   const Store = useSelector((store) => store.tours);
   useEffect(() => {
-    document.title = dispatch(fetchApiDataTours());
+    document.title = dispatch(fetchApiDataTours(token));
   }, []);
 
   const Reserve = (e) => {
     e.preventDefault();
-    dispatch(PostReservationsAPI(value, user));
-    navigate('/');
+    dispatch(PostReservationsAPI(value));
   };
   return (
     <div className="reservations">
