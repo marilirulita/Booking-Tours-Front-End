43e3fa30036dd91b7130592d6133e4ce8f270643
mar <marilirulita@gmail.com>
2022-09-21 03:44:50 -0700

    install swiper and configure carrousel in main

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,17 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
+// import Swiper for carrousel
+import {
+  Navigation, Pagination, Scrollbar, A11y,
+} from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
 import { fetchApiDataTours } from '../redux/tours/toursAPI';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 
 const Main = () => {
   const tours = useSelector((store) => store.tours);
@@ -11,25 +21,55 @@ const Main = () => {
   }, [dispatch]);
 
   return (
-    <div className="container">
+    <div className="main-container">
       <header>
-        <h1>Latest Tours</h1>
+        <h1>LATEST TOURS</h1>
         <p>Please select a Tour</p>
       </header>
-      <div>
-        <h3>Here is the carrusel of options</h3>
+      <Swiper
+        className="slide-container"
+      // install Swiper modules
+        modules={[Navigation, Pagination, Scrollbar, A11y]}
+        spaceBetween={25}
+        slidesPerView={3}
+        loop
+        grabCursor="true"
+        navigation
+        pagination={{ clickable: true }}
+        scrollbar={{ draggable: true }}
+        breakpoints={{
+          0: {
+            slidesPerView: 1,
+          },
+          750: {
+            slidesPerView: 2,
+          },
+          1100: {
+            slidesPerView: 3,
+          },
+        }}
+      >
         {tours.map((tour) => (
-          <div key={tour.id} className="element">
-            <img
-              src={tour.photo}
-              alt={tour.title}
-            />
-            <h3>{tour.title}</h3>
-            <p>{tour.description}</p>
-            <Link to={`/TourDetails/${tour.id}`}>Book Now</Link>
-          </div>
+          <SwiperSlide key={tour.id}>
+            <Link to={`/TourDetails/${tour.id}`}>
+              <div className="image-content">
+                <div className="card-image">
+                  <img
+                    src={tour.photo}
+                    alt={tour.title}
+                    className="card-img"
+                  />
+                </div>
+              </div>
+              <div className="card-content">
+                <h3 className="name">{tour.title}</h3>
+                <span className="dots-division">...............</span>
+                <p className="description">{tour.description}</p>
+              </div>
+            </Link>
+          </SwiperSlide>
         ))}
-      </div>
+      </Swiper>
     </div>
   );
 };
