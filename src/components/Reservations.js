9e2e1c7ca78bea188f9d6a170042266c52ff72dc
devMahmoud <eng.mahmoud.mohamed93@gmail.com>
2022-09-23 00:42:55 +0200

    Display reservation date and number of persons

--- src/components/Reservations.js
+++ src/components/Reservations.js
@@ -12,25 +12,33 @@ const Reservations = () => {
   }, []);
 
   return (
-    reservations.map((tour) => (
-      <div className="tour-details" key={tour.id}>
+    reservations.map((obj) => (
+      <div className="tour-details" key={obj.tour.id}>
         <div className="tour-photo">
-          <img src={tour.photo} alt="Tour" />
+          <img src={obj.tour.photo} alt="Tour" />
         </div>
         <div className="tour-data">
-          <h2 className="tour-title">{tour.title}</h2>
-          <p className="tour-description">{tour.description}</p>
+          <h2 className="tour-title">{obj.tour.title}</h2>
+          <p className="tour-description">{obj.tour.description}</p>
           <div className="tour-city">
             <p>City:</p>
-            <p>{tour.city}</p>
+            <p>{obj.tour.city}</p>
           </div>
           <div className="tour-duration">
             <p>Duration (weeks):</p>
-            <p>{tour.duration}</p>
+            <p>{obj.tour.duration}</p>
           </div>
           <div className="tour-cost">
             <p>Cost:</p>
-            <p>{tour.cost}</p>
+            <p>{obj.tour.cost}</p>
+          </div>
+          <div className="tour-duration num-people">
+            <p>Number of persons:</p>
+            <p>{obj.persons_number}</p>
+          </div>
+          <div className="tour-cost">
+            <p>Reservation Date:</p>
+            <p>{obj.reservation_date}</p>
           </div>
           <div className="tour-reserve-btn">
             <button type="button">Cancel Reservation</button>
