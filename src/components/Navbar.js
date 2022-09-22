d8925635ead568e5626032c1c462029e7059958c
devMahmoud <eng.mahmoud.mohamed93@gmail.com>
2022-09-23 00:41:32 +0200

    Add toggle button for navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,32 @@
 import { NavLink } from 'react-router-dom';
-// import logoUrl from '../img/logo.png';
+import React, { useState } from 'react';
 
 const Navbar = () => {
   const activeLink = ({ isActive }) => `nav-link${(isActive ? ' activated' : '')}`;
+  const [navbarOpen, setNavbarOpen] = useState(false);
+  const handleToggle = () => {
+    setNavbarOpen((prev) => !prev);
+  };
+
+  const closeMenu = () => {
+    setNavbarOpen(true);
+  };
 
   return (
     <nav>
-      <div className="navbar-content">
-        <div className="logo-container">
-          <h1 className="app-title">App Name</h1>
-        </div>
-        <div className="nav-links">
-          <NavLink to="/" className="nav-link">Home</NavLink>
-          <NavLink to="/Reservations" className={activeLink}>My Reservations</NavLink>
-          <NavLink to="/NewTour" className={activeLink}>Create Tour</NavLink>
-          <NavLink to="/ReservationsForm" className={activeLink}>Reserve Tour</NavLink>
-          <NavLink to="/DeleteTours" className={activeLink}>Delete Tour</NavLink>
-        </div>
+      <div className="btn-container">
+        { navbarOpen
+          ? <button className="menu-btn" onClick={handleToggle} type="button">&#9776;</button> : <button className="menu-btn" onClick={handleToggle} type="button">&#9747;</button>}
+      </div>
+      <div className={`logo-container ${navbarOpen ? ' closeMenu' : ''}`}>
+        <h1 className="app-title">App Name</h1>
+      </div>
+      <div className={`nav-links ${navbarOpen ? ' closeMenu' : ''}`}>
+        <NavLink to="/" className="nav-link" onClick={() => closeMenu()}>Home</NavLink>
+        <NavLink to="/Reservations" className={activeLink} onClick={() => closeMenu()}>My Reservations</NavLink>
+        <NavLink to="/NewTour" className={activeLink} onClick={() => closeMenu()}>Create Tour</NavLink>
+        <NavLink to="/ReservationsForm" className={activeLink} onClick={() => closeMenu()}>Reserve Tour</NavLink>
+        <NavLink to="/DeleteTours" className={activeLink} onClick={() => closeMenu()}>Delete Tour</NavLink>
       </div>
     </nav>
   );
