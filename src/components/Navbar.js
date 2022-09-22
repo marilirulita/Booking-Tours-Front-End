09382b237c92213ff4539c8bbd413a7f8deb29bc
devMahmoud <eng.mahmoud.mohamed93@gmail.com>
2022-09-22 18:00:18 +0200

    Fix my resrvations link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,16 +8,14 @@ const Navbar = () => {
     <nav>
       <div className="navbar-content">
         <div className="logo-container">
-          <h1 className="app-title">Space Travelers&apos; Hub</h1>
+          <h1 className="app-title">App Name</h1>
         </div>
         <div className="nav-links">
-          <NavLink to="/" className={activeLink}>Home</NavLink>
-          <NavLink to="/Reservations/:userID" className={activeLink}>My Reservations</NavLink>
-          <NavLink to="/NewTour" className={activeLink}>Create New Tour</NavLink>
+          <NavLink to="/" className="nav-link">Home</NavLink>
+          <NavLink to="/Reservations" className={activeLink}>My Reservations</NavLink>
+          <NavLink to="/NewTour" className={activeLink}>Create Tour</NavLink>
           <NavLink to="/ReservationsForm" className={activeLink}>Reserve Tour</NavLink>
           <NavLink to="/DeleteTours" className={activeLink}>Delete Tour</NavLink>
-          <NavLink to="/SignUp" className={activeLink}>Sign Up</NavLink>
-          <NavLink to="/Login" className={activeLink}>Login</NavLink>
         </div>
       </div>
     </nav>
