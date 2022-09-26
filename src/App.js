e3c9394cb879b756434955b8471db03c0ccd9ec6
devMahmoud <eng.mahmoud.mohamed93@gmail.com>
2022-09-26 23:32:22 +0200

    Remove counter

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Navbar from './components/Navbar';
 import Login from './components/Login';
 import SignUp from './components/SignUp';
 import Main from './components/Main';
-import { Counter } from './components/Counter';
 import TourDetails from './components/TourDetails';
 import NewTour from './components/NewTour';
 import ReservationsForm from './components/ReservationsForm';
@@ -25,7 +24,6 @@ function App() {
         <Route path="/" element={<Main />} />
         <Route path="/Login" element={<Login />} />
         <Route path="/SignUp" element={<SignUp />} />
-        <Route path="/counter" element={<Counter />} />
         <Route path="/TourDetails/:tourID" element={<TourDetails />} />
         <Route path="/NewTour" element={<NewTour />} />
         <Route path="/ReservationsForm" element={<ReservationsForm />} />
